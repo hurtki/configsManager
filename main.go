27ba0e90fad4cba184e888a363fee028df6a74c9
uno/hurtki <asboba2101@gmail.com>
2025-06-22 00:28:53 +0300

    create --cat flag and commad.

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 
 	c "github.com/hurtki/configManager/config"
 	"github.com/hurtki/configManager/commands"
+	"github.com/hurtki/configManager/utils"
 )
 
 func main() {
 	// cm config manager 
-	cfg, err := c.GetConfig()
+	_, err := c.GetConfig()
 	if err != nil {
 		fmt.Println("error", err.Error())
 		panic(err)
 	}
-	fmt.Println("cm config manager was got succsesfully:", cfg.RepoPath)
+	//fmt.Println("cm config manager was got succsesfully:", cfg.RepoPath)
 
 	// getting command arguments
 	args := os.Args
@@ -23,7 +24,7 @@ func main() {
 	
 	// commads handling
 	switch args[1]{
-	case "--path":
+	case "--path", "-p":
 		if argsLen < 3 {
 			fmt.Println("not enough args")
 			os.Exit(1)
@@ -36,5 +37,34 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println(value)
+	
+
+	case "--cat", "-c":
+		if argsLen < 3 {
+			fmt.Println("not enough args")
+			os.Exit(1)
+		}
+		key := args[2]
+
+		value, err := commands.GetPathConfigPath(key)
+		if err != nil {
+			fmt.Println("config was not found")
+			os.Exit(1)
+		}
+	
+		data, err := utils.GetFileText(value)
+		if err != nil {
+			fmt.Println("file from config list not found")
+			os.Exit(1)
+		}
+		fmt.Println(data)
+	
+	
+
+
+
+
+
+	
 	}
 }
---
+++ utils/file.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"errors"
+	"os"
+)
+
+
+
+func GetFileText(filepath string) (string, error) {
+	text, err := os.ReadFile(filepath)
+	if err != nil {
+		return "", errors.New("file not found")
+	}
+	return string(text), nil
+}
