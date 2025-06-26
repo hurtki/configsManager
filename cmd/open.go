195d9a84af57bd30f9fc18308c9bd54e07a24ab2
uno/hurtki <asboba2101@gmail.com>
2025-06-26 21:51:41 +0300

    add description for open command

--- cmd/open.go
+++ cmd/open.go
@@ -13,13 +13,17 @@ import (
 // openCmd represents the open command
 var openCmd = &cobra.Command{
 	Use:   "open [filename]",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Opens a config by name",
+	Long: `The 'open' command launches the default text editor specified in the application configuration 
+to open the configuration file associated with the provided key.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This is useful for quickly editing or viewing the content of config files without manually locating them.
+
+Usage example:
+  cm open myconfig
+
+This command will open the file linked to 'myconfig' in the configured editor, 
+you can configure it in app config by running 'cm open cm_config'`,
 	Run: func(cmd *cobra.Command, args []string) {
 		err := service.OpenByKey(args[0])
 		if err != nil {
