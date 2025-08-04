83e67d6b42e9d93f62903f4b817c984483908c5b
uno/hurtki <asboba2101@gmail.com>
2025-08-04 16:07:27 +0300

    add ( error handling in main )

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ Copyright © 2025 Alexey asboba2101@gmail.com >
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/hurtki/configsManager/cmd"
 	"github.com/hurtki/configsManager/services"
 )
@@ -17,5 +20,8 @@ func main() {
 
 	rootCmd := cmd.NewRootCmd(AppConfigService, StdInputService, ConfigsListService, OsService)
 
-	rootCmd.Execute()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
