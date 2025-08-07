28289f9ddb831f55bcd000cb9a1ff9491ea19afa
uno/hurtki <asboba2101@gmail.com>
2025-08-07 23:58:11 +0300

    add ( test with pipe only input for mainCmd )

--- cmd/add_test.go
+++ cmd/add_test.go
@@ -69,7 +69,7 @@ func TestAddCmd_ValidConfigAddTwoParam(t *testing.T) {
 }
 
 // test for valid config adding no args with pipe WIP
-/*
+
 func TestAddCmd_ValidConfigAddNoParamWithPipe(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
@@ -79,15 +79,19 @@ func TestAddCmd_ValidConfigAddNoParamWithPipe(t *testing.T) {
 	mockInputService := mocks.NewMockInputService(ctrl)
 	mockOsService := mocks.NewMockOsService(ctrl)
 
-	args := []string{"config", "config.json"}
-	mockInputService.EXPECT().GetPipedInput().Return("", false)
+	args := []string{}
+	pipe := "config.json\n"
+
+	mockInputService.EXPECT().GetPipedInput().Return(pipe, true)
 	returnAppConfig := services.NewDefaultAppConfig()
 	returnConfigsList := services.GetDefaultConfigsList("")
 
 	mockAppConfigService.EXPECT().Load().Return(returnAppConfig, nil)
 	mockConfigsListService.EXPECT().Load().Return(returnConfigsList, nil)
 
-	mockOsService.EXPECT().FileExists("config.json").Return(true, nil)
+	mockOsService.EXPECT().FileExists("config.json\n").Return(true, nil)
+
+	mockOsService.EXPECT().GetAbsolutePath("config.json\n").Return("home/usr/config.json", nil)
 
 	mockConfigsListService.EXPECT().Save(returnConfigsList).Return(nil)
 	addCmd := cmd.NewAddCmd(mockAppConfigService, mockInputService, mockConfigsListService, mockOsService)
@@ -95,4 +99,4 @@ func TestAddCmd_ValidConfigAddNoParamWithPipe(t *testing.T) {
 	if err != nil {
 		t.Errorf("excpected no error while adding, got %d", err)
 	}
-} */
+}
